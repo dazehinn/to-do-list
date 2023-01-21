2df1946ec42e480687c66690d35671d5bb6daa47
dazehinn <dazehinn@yahoo.fr>
2023-01-21 20:23:02 +0100

    source file

--- src/index.js
+++ src/index.js
@@ -1,64 +1,24 @@
 import './style.css';
+import {
+  newTask, tasksArray, addItem, populateListItem, clearTodo,
+} from './crud.js';
 
-const ToD = document.querySelector('.todo');
-const tasks = [
-  {
-    description: 'Wash dishes',
-    completed: true,
-    index: 1,
-  },
-  {
-    description: 'Double-tap to edit',
-    completed: false,
-    index: 5,
-  },
-  {
-    description: 'Build ToDo list structure',
-    completed: true,
-    index: 2,
-  },
-  {
-    description: 'Manage all your list in one place',
-    completed: false,
-    index: 3,
-  },
-  {
-    description: 'Resync to cleasr out the old',
-    completed: false,
-    index: 4,
-  },
-  {
-    description: 'Visit family',
-    completed: true,
-    index: 4,
-  },
-  {
-    description: 'Complete ToDo List project',
-    completed: false,
-    index: 8,
-  },
-];
-
-const todoInstr = document.createElement('ii');
-todoInstr.textContent = 'Add to your list...';
-todoInstr.classList.add('todoli');
-todoInstr.style.fontStyle = 'italic';
-ToD.append(todoInstr);
-
-function populateListItem() {
-  tasks.sort((a, b) => (a.index - b.index));
-  for (let i = 0; i < tasks.length; i += 1) {
-    const itemdiv = document.createElement('div');
-    const item = document.createElement('ii');
-    const checkbox = document.createElement('input');
-    item.textContent = tasks[i].description;
-    itemdiv.classList.add('todoli');
-    checkbox.type = 'checkbox';
-    checkbox.checked = tasks[i].completed;
-    itemdiv.append(checkbox, item);
-    checkbox.value = tasks[i].completed;
-    ToD.append(itemdiv);
-  }
-}
+const removeBtn = document.querySelector('button');
+const todoInstr = document.querySelector('.todoInstr');
 
 window.addEventListener('load', populateListItem);
+newTask.addEventListener('keypress', function (e) {
+  if (e.key === 'Enter' && todoInstr.value.length > 0) {
+    e.preventDefault();
+    tasksArray.push({
+      description: this.value,
+      completed: false,
+      index: tasksArray.length + 1,
+    });
+    localStorage.setItem('tasks', JSON.stringify(tasksArray));
+    addItem();
+    newTask.value = '';
+  }
+});
+
+removeBtn.onclick = clearTodo;
